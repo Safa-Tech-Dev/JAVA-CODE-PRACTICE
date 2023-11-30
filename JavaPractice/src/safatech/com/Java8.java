126f99f399ebbbb05e796da2d9c95ae361a44e25
Saddam-Pc <saddam8101.sk@gmail.com>
2023-11-30 21:24:33 +0530

    Given a list of integers, find out all the even numbers that exist in the list using Stream functions

--- JavaPractice/src/safatech/com/Java8.java
+++ JavaPractice/src/safatech/com/Java8.java
@@ -7,6 +7,10 @@ import java.util.stream.Collectors;
 public class Java8 {
 
     public static void main(String[] args) {
+        /*
+        Given a list of integers,
+        find out all the even numbers that exist in the list using Stream functions
+         */
         Integer[] arr = {1,1,3,6,0,7,-8,-1,-3};
         List<Integer> list = Arrays.asList(arr);
         List<Integer> l = list.stream().filter(i -> i % 2 == 0 ).collect(Collectors.toList());
