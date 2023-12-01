764bcdbbab208b27663c6bfc27ceb8d5173cc811
Saddam-Pc <saddam8101.sk@gmail.com>
2023-12-01 21:15:52 +0530

    1. Given a list of integers,         find out all the even numbers that exist in the list using Stream functions,2. Given a list of integers, find out all the numbers starting with 1 using Stream functions?

--- JavaPractice/src/safatech/com/Java8.java
+++ JavaPractice/src/safatech/com/Java8.java
@@ -8,13 +8,20 @@ public class Java8 {
 
     public static void main(String[] args) {
         /*
-        Given a list of integers,
+        1. Given a list of integers,
         find out all the even numbers that exist in the list using Stream functions
          */
         Integer[] arr = {1,1,3,6,0,7,-8,-1,-3};
-        List<Integer> list = Arrays.asList(arr);
-        List<Integer> l = list.stream().filter(i -> i % 2 == 0 ).collect(Collectors.toList());
-        System.out.println(l);
-        list.stream().filter(i -> i % 2 == 0).forEach(System.out::println);
+        List<Integer> myList1 = Arrays.asList(arr);
+        List<Integer> evenList = myList1.stream().filter(i -> i % 2 == 0 ).collect(Collectors.toList());
+        System.out.println(evenList);
+        myList1.stream().filter(i -> i % 2 == 0).forEach(System.out::println);
+
+        /*
+        2. Given a list of integers, find out all the numbers starting with 1 using Stream functions?
+         */
+        List<Integer> myList2 = Arrays.asList(10,12,15,8,9,7,5);
+        myList2.stream().map(s -> s + "") // converting to string
+                .filter( i -> i.startsWith("1")).forEach( i -> System.out.println(i));
     }
 }
