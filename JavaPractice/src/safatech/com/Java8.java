97210f6b42b9b93f37d06a0f7a09d210350273d4
Saddam-Pc <saddam8101.sk@gmail.com>
2023-12-04 20:09:59 +0530

    added Given the list of integers, find the first element of the list using Stream functions

--- JavaPractice/src/safatech/com/Java8.java
+++ JavaPractice/src/safatech/com/Java8.java
@@ -1,9 +1,6 @@
 package safatech.com;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Java8 {
@@ -13,6 +10,7 @@ public class Java8 {
         1. Given a list of integers,
         find out all the even numbers that exist in the list using Stream functions
          */
+        System.out.println("************ find all even numbers ************");
         Integer[] arr = {1,1,3,6,0,7,-8,-1,-3};
         List<Integer> myList1 = Arrays.asList(arr);
         List<Integer> evenList = myList1.stream().filter(i -> i % 2 == 0 ).collect(Collectors.toList());
@@ -22,6 +20,7 @@ public class Java8 {
         /*
         2. Given a list of integers, find out all the numbers starting with 1 using Stream functions?
          */
+        System.out.println("******* find all the number starting with 1");
         List<Integer> myList2 = Arrays.asList(10,12,15,8,9,7,5);
         myList2.stream().map(s -> s + "") // converting to string
                 .filter( i -> i.startsWith("1")).forEach( i -> System.out.println(i));
@@ -35,5 +34,21 @@ public class Java8 {
         Set<Integer> mySet = new HashSet<>();
         myList3.stream().filter(n -> !mySet.add(n)).forEach(System.out::println);
 
+        /*
+        Given the list of integers, find the first element of the list using Stream functions?
+         */
+        System.out.println("********** Find first Element of the list");
+
+        // method -1
+        Optional optional = myList3.stream().filter(i -> i > 12).findFirst();
+        if(optional.isPresent()){
+            System.out.println(optional.get());
+        }else {
+            System.out.println("No value present");
+        }
+        //method -2
+        myList3.stream().findFirst().ifPresent(System.out::println);
+
     }
+
 }
