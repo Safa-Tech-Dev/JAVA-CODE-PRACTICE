42679f893a8705e8fef78aca17ba0361e7658381
Saddam-Pc <saddam8101.sk@gmail.com>
2023-12-08 17:03:06 +0530

    added How can you synchronize an ArrayList in Java method

--- JavaPractice/src/safatech/com/CollectionPractice.java
+++ JavaPractice/src/safatech/com/CollectionPractice.java
@@ -2,16 +2,16 @@ package safatech.com;
 
 import javax.sound.midi.Soundbank;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class CollectionPractice {
 
 
     public static void main(String[] args) {
 
+        //How can you make an ArrayList read-only
+        System.out.println("****** ArrayList Read Only ***********");
         try{
             List<String> l = new ArrayList<>();
             l.add("practice");
@@ -25,6 +25,8 @@ public class CollectionPractice {
             System.out.println("Exception thrown is: " + e);
         }
 
+        // use of Properties class
+        System.out.println("**** Properties Class *******");
         try{
             FileReader file = new FileReader("info.properties");
             Properties obj_file = new Properties();
@@ -34,5 +36,37 @@ public class CollectionPractice {
         }catch (Exception e ){
             System.out.println("File NotFound" + e);
         }
-    }
+
+        // How can you synchronize an ArrayList in Java?
+        //method - 1 (Using Collections.synchronizedList() method:)
+        System.out.println("****** Synchronized List ********");
+        List<String> synchronized_list = Collections.synchronizedList(new ArrayList<>());
+        synchronized_list.add("Learn");
+        synchronized_list.add("Solve");
+        synchronized_list.add("Practice");
+        synchronized_list.add("Interview");
+        synchronized (synchronized_list){ // Must be declared
+
+            Iterator it = synchronized_list.iterator();
+            while (it.hasNext()){
+                System.out.println(it.next());
+            }
+        }
+        //method - 2 (Using CopyOnWriteArrayList)
+
+        //Creating a thread-safe ArrayList
+        System.out.println("******* Using CopyOnWriteArrayList ********");
+        CopyOnWriteArrayList<String> syn_list = new CopyOnWriteArrayList<>();
+
+        syn_list.add("Learn");
+        syn_list.add("Practice");
+        syn_list.add("Solve");
+        syn_list.add("Interview");
+
+        Iterator<String> it = syn_list.iterator();
+        while (it.hasNext()){
+            System.out.println(it.next());
+            syn_list.add("Saddam");
+        }
+     }
 }
