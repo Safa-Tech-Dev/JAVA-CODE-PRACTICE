623679adfb692ae82f7b6f3f993abb960fd71206
Saddam-Pc <saddam8101.sk@gmail.com>
2024-05-16 16:16:15 +0530

    interrupted and interrupt method

---
+++ JavaPractice/src/com/safatech/thread1/TestInterruptingThread4.java
@@ -0,0 +1,22 @@
+package com.safatech.thread1;
+// interrupted and interrupt method
+public class TestInterruptingThread4 extends Thread{
+
+    public void run(){
+        for(int i = 1; i<=5; i++){
+            if(Thread.interrupted()){
+                System.out.println("Code for Interrupted Thread...");
+            }else {
+                System.out.println("Code for Normal Thread...");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        TestInterruptingThread4 t1 = new TestInterruptingThread4();
+        TestInterruptingThread4 t2 = new TestInterruptingThread4();
+        t1.start();
+        t1.interrupt();
+        t2.start();
+    }
+}
