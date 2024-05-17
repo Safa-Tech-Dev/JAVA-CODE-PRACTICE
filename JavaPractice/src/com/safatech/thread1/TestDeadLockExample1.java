085e7531e51cbe062e96a6bcb44651508c715589
Saddam-Pc <saddam8101.sk@gmail.com>
2024-05-17 16:36:34 +0530

    added TestDeadLockExample1

---
+++ JavaPractice/src/com/safatech/thread1/TestDeadLockExample1.java
@@ -0,0 +1,46 @@
+package com.safatech.thread1;
+
+public class TestDeadLockExample1 {
+
+    public static void main(String[] args) {
+        final String resource1 = "Saddam";
+        final String resource2 = "Hassan";
+
+        //t1 tries to lock resource1 and then resource2
+        Thread t1 = new Thread(){
+            public void run(){
+                synchronized (resource1){
+                    System.out.println("Thread1: locked resource1");
+                    try{
+                        Thread.sleep(100);
+                    }catch (Exception e){
+
+                    }
+                    synchronized (resource2){
+                        System.out.println("Thread1: locked resource2");
+                    }
+                }
+            }
+        };
+
+        //t2 tries to lock resource2 then resource1
+        Thread t2 = new Thread(){
+            public void run(){
+                synchronized (resource2){
+                    System.out.println("Thread2: locked resource2");
+                    try{
+                        Thread.sleep(100);
+                    }catch (Exception e){
+
+                    }
+                    synchronized (resource1){
+                        System.out.println("Thread2: locked resource1");
+                    }
+                }
+            }
+        };
+
+        t1.start();
+        t2.start();
+    }
+}
