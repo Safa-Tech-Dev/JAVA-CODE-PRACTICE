a2217f3a5d5579b4e12a7318cfa55782c78368b9
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2024-08-30 10:07:53 +0530

    added parallel streaam method

---
+++ JavaPractice/src/com/safatech/java8/ParallelStreamExample.java
@@ -0,0 +1,56 @@
+package com.safatech.java8;
+
+import com.safatech.java8.dao.EmployeeDao;
+import com.safatech.java8.model.Employee;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class ParallelStreamExample {
+
+    public static void main(String[] args) {
+
+       long start = 0;
+        long end = 0;
+        /*
+        start = System.currentTimeMillis();
+        IntStream.range(1, 100).forEach(System.out::println);
+        end = System.currentTimeMillis();
+        System.out.println("Squential stream took: " + (end - start) + " ms");
+
+        System.out.println("=============================================");
+
+        start = System.currentTimeMillis();
+        IntStream.range(1, 100).parallel().forEach(System.out::println);
+        end = System.currentTimeMillis();
+        System.out.println("Parallel  stream took: " + (end - start) + " ms");*/
+
+      /*  IntStream.range(1, 10)
+                .forEach(x -> {
+                    System.out.println(" Thread " + Thread.currentThread().getName() + " : " + x);
+                });
+        System.out.println("======================================================");
+        IntStream.range(1, 10)
+                .parallel()
+                .forEach(x -> {
+                    System.out.println(" Thread " + Thread.currentThread().getName() + " : " + x);
+                });*/
+
+        List<Employee> employeeList = EmployeeDao.getEmployees();
+        start = System.currentTimeMillis();
+        double avgSalaryStream = employeeList.stream()
+                .map(Employee::getSalary)
+                .mapToDouble(i -> i)
+                .average().getAsDouble();
+        end = System.currentTimeMillis();
+
+        System.out.println("Normal stream execution Time: " + (end - start) + " : avg salary: " + avgSalaryStream);
+        start = System.currentTimeMillis();
+        double avgSalaryWithParallelStream = employeeList.parallelStream()
+                .map(Employee::getSalary)
+                .mapToDouble(i -> i)
+                .average().getAsDouble();
+        end = System.currentTimeMillis();
+        System.out.println("Normal stream execution Time: " + (end - start) + " : avg salary: " + avgSalaryWithParallelStream);
+    }
+}
--- JavaPractice/src/com/safatech/java8/dao/EmployeeDao.java
+++ JavaPractice/src/com/safatech/java8/dao/EmployeeDao.java
@@ -3,16 +3,21 @@ package com.safatech.java8.dao;
 import com.safatech.java8.model.Employee;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class EmployeeDao {
 
    public static List<Employee> getEmployees(){
        List<Employee> employeeList = new ArrayList<>();
-       employeeList.add(new Employee(176, "Roshan" , "IT", "A", 600000));
+        /*  employeeList.add(new Employee(176, "Roshan" , "IT", "A", 600000));
        employeeList.add(new Employee(388, "Saddam" , "ECE", "B", 1000000));
        employeeList.add(new Employee(378, "Aquib" , "CSE", "A",500000));
        employeeList.add(new Employee(889, "Shahbaz" , "Customer support", "A",400000));
        employeeList.add(new Employee(569, "Asif" , "ECE", "C",1200000));
+       return employeeList;*/
+       for(int i = 1 ; i < 1000; i++){
+           employeeList.add(new Employee(i , "emp"+i,"xxx"+i, "A"+i, Long.valueOf(new Random().nextInt(1000 * 100))));
+       }
        return employeeList;
    }
 }
