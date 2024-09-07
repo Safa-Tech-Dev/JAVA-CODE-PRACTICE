1cf133a9d6bcd95b01e919fc5c4982d3d735f3ae
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2024-09-07 19:53:59 +0530

    added Find All Even method

--- JavaPractice/src/com/safatech/java8/FindAllEven.java
+++ JavaPractice/src/com/safatech/java8/FindAllEven.java
@@ -1,2 +1,21 @@
-package com.safatech.java8;public class FindAllEven {
+package com.safatech.java8;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/*
+     1. Given a list of integers,
+     find out all the even numbers that exist in the list using Stream functions
+ */
+public class FindAllEven {
+
+    public static void main(String[] args) {
+
+        Integer[] arr = {1,1,3,6,0,7,-8,-1,-3};
+        List<Integer> numList = Arrays.asList(arr);
+        List<Integer> evenList = numList.stream().filter(i -> i % 2 == 0).collect(Collectors.toList());
+        System.out.println(evenList);
+        numList.stream().filter(i -> i % 2 == 0).forEach( x -> System.out.println(x));
+    }
 }
