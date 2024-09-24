3e78ed112e32cb19518656760867c0c9157f4292
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2024-09-24 22:47:11 +0530

    added SupplyAsync method

---
+++ JavaPractice/src/com/safatech/java8/SupplyAsyncDemo.java
@@ -0,0 +1,32 @@
+package com.safatech.java8;
+
+import com.safatech.java8.dao.EmployeeJsonDB;
+import com.safatech.java8.model.EmployeeJson;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class SupplyAsyncDemo {
+
+    public List<EmployeeJson> getEmployees() throws ExecutionException, InterruptedException {
+
+        Executor executor = Executors.newCachedThreadPool();
+
+        CompletableFuture<List<EmployeeJson>> listCompletableFuture = CompletableFuture
+                .supplyAsync(() -> {
+                    System.out.println("Thread: " + Thread.currentThread().getName());
+            return EmployeeJsonDB.fetchEmployee();
+        }, executor);
+        return listCompletableFuture.get();
+    }
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        SupplyAsyncDemo supplyAsyncDemo = new SupplyAsyncDemo();
+        List<EmployeeJson> supplyAsyncDemoEmployees = supplyAsyncDemo.getEmployees();
+        supplyAsyncDemoEmployees.stream().forEach(System.out::println);
+
+    }
+}
---
+++ JavaPractice/src/com/safatech/java8/dao/EmployeeJsonDB.java
@@ -0,0 +1,23 @@
+package com.safatech.java8.dao;
+
+import com.safatech.java8.model.EmployeeJson;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class EmployeeJsonDB {
+
+    public static git List<EmployeeJson> fetchEmployee(){
+        ObjectMapper mapper = new ObjectMapper();
+       try {
+           return
+               mapper.readValue(new File("employees.json"), new TypeReference<List<EmployeeJson>>(){
+
+               });
+        }catch (IOException e){
+           e.printStackTrace();
+       }
+       return null;
+    }
+}
