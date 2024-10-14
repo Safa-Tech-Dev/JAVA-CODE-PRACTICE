88d0b72f78fa6dfb8d49306ab66c0e86be6ca1ff
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2024-10-14 23:14:55 +0530

    added last occurrence method

--- JavaPractice/src/com/safatech/dsa/datastructure/array/LastOccurrence.java
+++ JavaPractice/src/com/safatech/dsa/datastructure/array/LastOccurrence.java
@@ -1,2 +1,29 @@
-package com.safatech.dsa.datastructure.array;public class LastOccurrence {
+package com.safatech.dsa.datastructure.array;
+/**
+ * Check the last occurrence of the element in an array
+ */
+public class LastOccurrence {
+
+    public static int lastOccurrence(int[] arr, int item){
+
+        int lastIndex = - 1;
+
+        for(int i = 0; i<arr.length; i++){
+
+            if(arr[i] == item){
+                lastIndex = i;
+            }
+        }
+        return lastIndex;
+    }
+    public static void main(String[] args) {
+
+        int[] arr1 = {5,2,6,5,8,9,5};
+        int item1 = 5;
+        System.out.println(lastOccurrence(arr1,item1));
+        int[] arr2 = {5,2,6,5,8,9,5};
+        int item2 = 7;
+        System.out.println(lastOccurrence(arr2,item2));
+
+    }
 }
