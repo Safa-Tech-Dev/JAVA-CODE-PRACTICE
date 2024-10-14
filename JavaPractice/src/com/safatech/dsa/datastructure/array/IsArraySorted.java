fcbaacab4ca8ded66e96015bed1b19a726105c35
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2024-10-14 22:23:49 +0530

    added IsSorted method

--- JavaPractice/src/com/safatech/dsa/datastructure/array/IsArraySorted.java
+++ JavaPractice/src/com/safatech/dsa/datastructure/array/IsArraySorted.java
@@ -1,2 +1,27 @@
-package com.safatech.dsa.datastructure.array;public class IsArraySorted {
+package com.safatech.dsa.datastructure.array;
+
+/**
+ * Check the array is sorted or not
+ */
+public class IsArraySorted {
+
+    public static boolean isSorted(int[] arr){
+
+        boolean isSorted = true;
+        for(int i = 1; i<arr.length; i++){
+
+            if(arr[i] < arr[i-1]){
+                isSorted = false;
+                break;
+            }
+        }
+        return isSorted;
+    }
+    public static void main(String[] args) {
+
+        int[] arr1 = {0,1,3,5,6,2,8,9,5}; //not sorted
+        System.out.println(isSorted(arr1));
+        int[] arr2 = {0,1,3,5,6,8,9};
+        System.out.println(isSorted(arr2)); //sorted
+    }
 }
