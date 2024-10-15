cd725d92b4eecd95ddee9ea9f4cbedc908d0305b
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2024-10-15 18:21:08 +0530

    added Smallest and Largest method

--- JavaPractice/src/com/safatech/dsa/datastructure/array/SmallestNLargestElement.java
+++ JavaPractice/src/com/safatech/dsa/datastructure/array/SmallestNLargestElement.java
@@ -2,9 +2,9 @@ package com.safatech.dsa.datastructure.array;
 
 import java.util.Arrays;
 
-public class SmallestNLargestIndex {
+public class SmallestNLargestElement {
 
-    public static int[] smallestNLargestIndex(int[] arr){
+    public static int[] smallestNLargestElement (int[] arr){
 
         Arrays.sort(arr);
         int[] ans = {arr[0], arr[arr.length - 1]};
@@ -13,7 +13,7 @@ public class SmallestNLargestIndex {
     public static void main(String[] args) {
 
         int[] arr = {0,1,3,5,6,8,9};
-        System.out.println(Arrays.toString(smallestNLargestIndex(arr)));
+        System.out.println(Arrays.toString(smallestNLargestElement(arr)));
 
     }
 }
