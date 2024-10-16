730c384ddb9418dd3a2f52a88ef3cd19242223f7
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2024-10-16 22:18:58 +0530

    added Target Sum OR Two Sum method

--- JavaPractice/src/com/safatech/dsa/datastructure/array/pattern/targetsum/TargetSumORTwoSum.java
+++ JavaPractice/src/com/safatech/dsa/datastructure/array/pattern/targetsum/TargetSumORTwoSum.java
@@ -1,5 +1,8 @@
-package com.safatech.dsa.datastructure.array;
+package com.safatech.dsa.datastructure.array.pattern.targetsum;
 
+/**
+ * find the total no. of pairs in the array whose sum is equal to the given value x.
+ */
 public class TargetSumORTwoSum {
 
     public static int targetSum(int[] arr, int target){
@@ -7,9 +10,8 @@ public class TargetSumORTwoSum {
         int ans = 0;
         int n = arr.length;
 
-        for(int i = 0; i<n; i++){
-
-            for(int j = i + 1; j<n; j++){
+        for(int i = 0; i<n; i++){ // 1st number
+            for(int j = i + 1; j<n; j++){ // 2nd number
                 if(arr[i] + arr[j] == target){
                     ans++;
                 }
