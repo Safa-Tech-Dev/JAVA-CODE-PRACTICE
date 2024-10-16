01a665dc4d12bdbfb734f4ec49199a71902ea560
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2024-10-16 18:08:16 +0530

    added Triplet Sum method

---
+++ JavaPractice/src/com/safatech/dsa/datastructure/array/TripletSum.java
@@ -0,0 +1,30 @@
+package com.safatech.dsa.datastructure.array;
+
+public class TripletSum {
+
+    public static int tripletSum(int[] arr, int target){
+
+        int n = arr.length;
+        int ans = 0;
+
+        for(int i = 0; i<n; i++){ // 1st number
+            for(int j = i + 1; j<n; j++){ //2nd number
+                for(int k = j + 1; k<n; k++){ //3rd number
+
+                    if(arr[i] + arr[j] + arr[k] == target){
+
+                        ans++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+
+        int[] arr = {1,4,5,6,3};
+        int target = 12;// {1,5,6}, {4,5,3}
+        System.out.println(tripletSum(arr,target));
+
+    }
+}
