d3e51be6200351c38c499cf3ea9961d959155914
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2024-10-16 11:29:05 +0530

    added Kth Smallest and  Largest Element

---
+++ JavaPractice/src/com/safatech/dsa/datastructure/array/KthSmallestNLargestElement.java
@@ -0,0 +1,20 @@
+package com.safatech.dsa.datastructure.array;
+
+import java.util.Arrays;
+
+public class KthSmallestNLargestElement {
+
+    public static int[] kthSmallestNLargestElement(int[] arr, int k){
+
+        Arrays.sort(arr);
+        int[] ans = {arr[k-1], arr[arr.length - k]};
+        return ans;
+
+    }
+    public static void main(String[] args) {
+
+        int[] arr = {3,2,1,5,6,4}; // 1,2,3,4,5,6
+        int k = 2;
+        System.out.println(Arrays.toString(kthSmallestNLargestElement(arr,k)));
+    }
+}
