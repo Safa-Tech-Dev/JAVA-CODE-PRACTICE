abb7faa4153b2bfdd2e0f614eb8e1a3dec71f84b
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2024-10-17 22:41:57 +0530

    added Find Second Largest element method

---
+++ JavaPractice/src/com/safatech/dsa/datastructure/array/pattern/arraymanipulation/FindSecondLargest.java
@@ -0,0 +1,38 @@
+package com.safatech.dsa.datastructure.array.pattern.arraymanipulation;
+
+/**
+ * find the second largest element in a given array
+ */
+public class FindSecondLargest {
+
+    public static int findMax(int[] arr){
+
+        int mx = Integer.MIN_VALUE; // assign the minimest value
+
+        for(int i = 0; i<arr.length; i++){ // loop through the array
+
+            if(arr[i] > mx){ // check element is greater than the max value
+                mx = arr[i]; // if true assign as max
+            }
+        }
+        return mx;
+    }
+
+    public static int findSecondMax(int[] arr){
+
+        int mx = findMax(arr); // maximum value
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] == mx){ //check array element is equal to max value
+                arr[i] = Integer.MIN_VALUE; // if true strike out, assign infinity
+            }
+        }
+        int secondMax = findMax(arr); // this will return second largest value
+        return secondMax;
+    }
+    public static void main(String[] args) {
+
+        int[] arr = {5,2,5,3,5};
+        System.out.println(findSecondMax(arr));
+
+    }
+}
