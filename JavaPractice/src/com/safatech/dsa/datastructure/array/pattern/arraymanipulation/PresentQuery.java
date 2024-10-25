299fb5ab03e5b7c62256b387d3fd4c541fafc92f
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2024-10-26 00:00:11 +0530

    added Present query frequency method

---
+++ JavaPractice/src/com/safatech/dsa/datastructure/array/pattern/arraymanipulation/PresentQuery.java
@@ -0,0 +1,40 @@
+package com.safatech.dsa.datastructure.array.pattern.arraymanipulation;
+
+import java.util.Scanner;
+
+public class PresentQuery {
+
+    public static int[] makeFrequencyArray(int[] arr){
+
+        int[] frequency = new int[100005];
+
+        for(int i = 0; i< arr.length; i++){
+
+            frequency[arr[i]]++;
+        }
+        return frequency;
+    }
+
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,2,3,4,5};
+
+        int[] freq = makeFrequencyArray(arr);
+
+        Scanner sc = new Scanner(System.in);
+        int queries = 5;
+        while(queries > 0){
+
+            System.out.println("Enter the number to be searched...");
+            int x = sc.nextInt();
+            if (freq[x] > 0){
+                System.out.println("YES");
+            }else{
+                System.out.println("NO");
+            }
+            queries--;
+        }
+
+    }
+}
