e4125fd90eabaf43789ae1837ca2150262106910
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2024-10-28 22:27:00 +0530

    added sort array with two pointers  approach

---
+++ JavaPractice/src/com/safatech/dsa/datastructure/array/pattern/twopointerers/SortArray.java
@@ -0,0 +1,68 @@
+package com.safatech.dsa.datastructure.array.pattern.twopointerers;
+
+public class SortArray {
+
+    public static void print(int[] arr){
+
+        for(int i = 0; i< arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void swap(int[] arr, int i, int  j){
+
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public static void sortZeroesAndOnes(int[] arr){
+
+        int n = arr.length;
+        int left = 0, right = n -1;
+        while (left < right){
+
+            if(arr[left] == 1 && arr[right] == 0){
+                swap(arr, left, right);
+                left++;
+                right--;
+            }
+            if(arr[left] == 0){
+                left++;
+            }
+            if(arr[right] == 1){
+                right--;
+            }
+        }
+    }
+   /* public static void sortZeroesAndOnes(int[] arr){
+
+        int zeroes = 0;
+        int n = arr.length;
+        //count number of zeroes
+        for(int i = 0; i < n; i++){
+            if(arr[i] == 0){
+                zeroes++;
+            }
+        }
+
+        // 0 to zeroes - 1: 0 and zeroes to n - 1: 1
+        for(int i = 0; i < n; i++){
+            // if i < count of zeroes
+            if(i< zeroes){
+                arr[i] = 0;
+            }else {
+                arr[i] = 1;
+            }
+        }
+
+    }*/
+    public static void main(String[] args) {
+
+        int[] arr = {1,0,1,0,1,0,1,0,1,0};
+        System.out.println("Original array:");
+        print(arr);
+        sortZeroesAndOnes(arr);
+        System.out.println("Sorted array:");
+        print(arr);
+    }
+}
