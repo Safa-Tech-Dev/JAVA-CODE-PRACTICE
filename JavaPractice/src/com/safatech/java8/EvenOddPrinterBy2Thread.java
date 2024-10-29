51dd61eeaecb481ec9202e8f1e0f8610f7b8312d
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2024-10-29 20:20:24 +0530

    added print even odd value using thread and Executor service

--- JavaPractice/src/com/safatech/java8/EvenOddPrinterBy2Thread.java
+++ JavaPractice/src/com/safatech/java8/EvenOddPrinterBy2Thread.java
@@ -1,2 +1,49 @@
-package com.safatech.java8;public class EvenOddPrinterBy2Thread {
+package com.safatech.java8;
+
+import java.util.stream.IntStream;
+
+public class EvenOddPrinterBy2Thread implements Runnable{
+
+    static int count = 1;
+    Object object;
+
+    //approach 1 with Java 7
+    public EvenOddPrinterBy2Thread(Object object) {
+        this.object = object;
+    }
+
+    @Override
+    public void run() {
+
+        while(count <= 10){
+            if(count % 2 == 0 && Thread.currentThread().getName().equals("even")){
+                synchronized (object){
+                    System.out.println("Thread name : " + Thread.currentThread().getName() + " value " + count);
+                    count++;
+                    try {
+                        object.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+            if(count % 2 != 0 && Thread.currentThread().getName().equals("odd")){
+                synchronized (object){
+                    System.out.println("Thread name : " + Thread.currentThread().getName() + " value " + count);
+                    count++;
+                    object.notify();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Object lock = new Object();
+        Runnable r1 = new EvenOddPrinterBy2Thread(lock);
+        Runnable r2 = new EvenOddPrinterBy2Thread(lock);
+        new Thread(r1, "even").start();
+        new Thread(r2, "odd").start();
+    }
 }
+
