a91f50ba593695812635d2cdd5775c719dead9da
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2024-11-03 22:58:53 +0530

    added Sort Array Squared method

---
+++ JavaPractice/src/com/safatech/dsa/datastructure/array/pattern/twopointerers/SortArraySquared.java
@@ -0,0 +1,47 @@
+package com.safatech.dsa.datastructure.array.pattern.twopointerers;
+
+public class SortArraySquared {
+
+    public static int[] sortSquared(int[] arr){
+
+        int n = arr.length,
+                left = 0,
+                right = n -1,
+                k = n - 1; // start filling the ans arr from right so that no need to reverse
+        int[] ans = new int[n];
+
+        while(left <= right){
+            if(Math.abs(arr[left]) > Math.abs(arr[right])){
+                ans[k--] = arr[left] * arr[left];
+                left++;
+            }else {
+                ans[k--] = arr[right] * arr[right];
+                right--;
+            }
+        }
+        return ans;
+    }
+    public static void print(int[] arr){
+
+        for(int i = 0; i< arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {-10,-5,1,2,3};
+        System.out.println("Original array:");
+        print(arr1);
+        int[] ans = sortSquared(arr1);
+        System.out.println("Sorted array:");
+        print(ans);
+
+        int[] arr2 = {-1,-2,-3,-4,-5,-6,-7};
+        System.out.println("Original array:");
+        print(arr2);
+        int[] ans2 = sortSquared(arr2);
+        System.out.println("Sorted array:");
+        print(ans2);
+    }
+}
