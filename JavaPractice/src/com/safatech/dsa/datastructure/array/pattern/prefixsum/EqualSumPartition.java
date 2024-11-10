a0d87d895dbdb18bc3e978a1509e1136cb6da4bb
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2024-11-10 17:52:17 +0530

    added Equal Sum Partition method

--- JavaPractice/src/com/safatech/dsa/datastructure/array/pattern/prefixsum/EqualSumPartition.java
+++ JavaPractice/src/com/safatech/dsa/datastructure/array/pattern/prefixsum/EqualSumPartition.java
@@ -1,2 +1,35 @@
-package com.safatech.dsa.datastructure.array.pattern.prefixsum;public class EqualSumPartition {
+package com.safatech.dsa.datastructure.array.pattern.prefixsum;
+
+public class EqualSumPartition {
+
+    public static int getArraySum(int[] arr){
+
+        int totalSum = 0;
+        for(int i = 0; i < arr.length; i++){
+
+            totalSum += arr[i];
+        }
+        return totalSum;
+    }
+    public static boolean getEqualSumPartition(int[] arr){
+
+        int totalSum = getArraySum(arr);
+        int prefixSum = 0;
+        for(int i = 0; i < arr.length; i++){
+            prefixSum += arr[i];
+            int suffixSum = totalSum - prefixSum;
+            if(prefixSum == suffixSum){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+
+        int[] arr1 = {1,2,3,4,5};
+        System.out.println("Equal partition possible? " + getEqualSumPartition(arr1));
+        int[] arr2 = {1,3,5,6,2,1};
+        System.out.println("Equal partition possible? " + getEqualSumPartition(arr2));
+
+    }
 }
