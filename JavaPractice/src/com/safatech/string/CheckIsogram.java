377def7546bfb2a84c689391111b45b504d50e7f
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2024-11-10 21:47:13 +0530

    added Check Isogram method

--- JavaPractice/src/com/safatech/string/CheckIsogram.java
+++ JavaPractice/src/com/safatech/string/CheckIsogram.java
@@ -1,2 +1,35 @@
-package com.safatech.string;public class CheckIsogram {
+package com.safatech.string;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+Printing Isogram or not(Which does not contain any duplicate characters)
+*/
+public class CheckIsogram {
+
+    public static void main(String[] args) {
+
+        String str = "codedecode";
+        System.out.println(isIsogram(str));
+        String str1 = "sky";
+        System.out.println(isIsogram(str1));
+
+    }
+
+    public static boolean isIsogram(String str){
+
+        char[] charArray = str.toCharArray();
+        Set<Character> characterSet = new HashSet<>();
+        boolean isogram = true;
+        for( Character c : charArray){
+
+            if(characterSet.contains(c)){
+                isogram = false;
+            }else{
+                characterSet.add(c);
+            }
+        }
+        return isogram;
+    }
 }
