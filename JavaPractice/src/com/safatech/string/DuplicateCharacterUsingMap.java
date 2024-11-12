923aacc1cbea91ae327f9154ca5c2dcd976dd76d
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2024-11-12 20:11:07 +0530

    added Duplicate Character Using Map method

---
+++ JavaPractice/src/com/safatech/string/DuplicateCharacterUsingMap.java
@@ -0,0 +1,40 @@
+package com.safatech.string;
+
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class DuplicateCharacterUsingMap {
+
+    public static Set<Character> getDuplicateCharacter(String str){
+
+        Set<Character> duplicates = new LinkedHashSet<>(); // LinkedHashSet() to maintain the order
+        Map<Character, Integer> chCount = new HashMap<>();
+
+        for(int i = 0; i < str.length(); i++){
+
+            if(chCount.containsKey(str.charAt(i))){
+                chCount.put(str.charAt(i), chCount.get(str.charAt(i)) + 1);
+            }else{
+                chCount.put(str.charAt(i), 1);
+            }
+        }
+        for(Map.Entry<Character, Integer> entry : chCount.entrySet()){
+
+            if(entry.getValue() > 1){
+                duplicates.add(entry.getKey());
+            }
+        }
+        return duplicates;
+    }
+    public static void main(String[] args) {
+
+        String s1 = "codedecode";
+        String s2 = "saddam";
+        String s3 = "farheen";
+        System.out.println(getDuplicateCharacter(s1));
+        System.out.println(getDuplicateCharacter(s2));
+        System.out.println(getDuplicateCharacter(s3));
+    }
+}
