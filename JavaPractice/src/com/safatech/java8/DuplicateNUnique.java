b09ccf8d9ebd74d856f21bff4d927c60435f614a
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2024-11-18 20:18:21 +0530

    added Number Starts With1 method

---
+++ JavaPractice/src/com/safatech/java8/DuplicateNUnique.java
@@ -0,0 +1,49 @@
+package com.safatech.java8;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class DuplicateElement {
+
+      /*
+       How to find duplicate elements in a given integers list in java using Stream functions?
+      */
+    public static void findDuplicateInArray(Integer[] arr){
+
+        Set<Integer> uniqueNumber = new HashSet<>();
+        List<Integer> integerList = Arrays.asList(arr);
+        Set<Integer> duplcateNumber = integerList.stream().filter(n -> !uniqueNumber.add(n)).collect(Collectors.toSet());
+        System.out.println("Original : " + Arrays.toString(arr));
+        System.out.println("Unique : " + uniqueNumber);
+        System.out.println("Duplicate : " + duplcateNumber);
+
+        // using Collections.frequency()
+        Set<Integer> duplicateUsingFreq = integerList.stream()
+                .filter(n -> Collections.frequency(integerList, n) > 1)
+                .collect(Collectors.toSet());
+        System.out.println("Duplicate using Frequecy: " + duplicateUsingFreq);
+
+    }
+    public static void findDupicateInString(String str){
+
+        Map<String, Long> mapOfLetter = Arrays.stream(str.split(""))
+                .map(String::toLowerCase)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        System.out.println(mapOfLetter);
+
+        List<String> duplicateLetter = mapOfLetter.entrySet().stream() // stream can not be applied to map, so convert it to entryset is used.
+                .filter(n -> n.getValue() > 1)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+        System.out.println("Duplicate letter : " + duplicateLetter);
+    }
+    public static void main(String[] args) {
+
+        Integer[] arr = new Integer[]{12,34,12,5,5,23};
+        findDuplicateInArray(arr);
+        System.out.println("============ String ===============");
+        String str = "ILoveDawateIslami";
+        findDupicateInString(str);
+    }
+}
---
+++ JavaPractice/src/com/safatech/string/NumberStartsWith1.java
@@ -0,0 +1,16 @@
+package com.safatech.string;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class NumberStartsWith1 {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = Arrays.asList(10,12,15,8,9,7,5);
+        list.stream()
+                .map(s -> s + "") // converting to string
+                .filter( i -> i.startsWith("1"))
+                .forEach(System.out::println);
+    }
+}
