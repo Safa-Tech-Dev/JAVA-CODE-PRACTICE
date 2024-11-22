4fbc55e1ba9debcbd2e669b0f1ccca27e00c81f6
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2024-11-23 00:35:42 +0530

    added Find nth fibonacci number method

--- JavaPractice/src/com/safatech/corejava/Fibonacci.java
+++ JavaPractice/src/com/safatech/corejava/Fibonacci.java
@@ -1,2 +1,21 @@
-package com.safatech.corejava;public class Fibonacci {
+package com.safatech.corejava;
+
+   /*
+   Finding the nth element in the Fibonacci series
+   */
+
+public class Fibonacci {
+
+    public static int getNthFibonacci(int n){
+
+        if(n <= 1)
+            return n;
+        return getNthFibonacci(n -1) + getNthFibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+
+        int num = 10;
+        System.out.println(getNthFibonacci(num));
+    }
 }
