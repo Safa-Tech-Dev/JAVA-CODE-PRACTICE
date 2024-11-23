bc3e95b6b249e8f66aa47740e4c68c5aa0824bf5
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2024-11-23 22:09:08 +0530

    added find the Firs tNon And Repeated Element in java 8

--- JavaPractice/src/com/safatech/string/FirstNonAndRepeatedElement.java
+++ JavaPractice/src/com/safatech/string/FirstNonAndRepeatedElement.java
@@ -2,11 +2,10 @@ package com.safatech.string;
 
 import java.util.Arrays;
 import java.util.LinkedHashMap;
-import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-public class FirstNonRepeatedElement {
+public class FirstNonAndRepeatedElement {
 
     public static void getFirstNonRepeated(String str){
 
@@ -18,8 +17,25 @@ public class FirstNonRepeatedElement {
                 .findFirst().get().getKey();
         System.out.println(firstNonRepeated);
     }
+
+     /*
+     Find First Repeated Element from the String
+     */
+    public static void getFirstRepeated(String str){
+
+        String firstRpeated = Arrays.stream(str.split(""))
+                .map(String::toLowerCase)
+                .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()))
+                .entrySet().stream()
+                .filter(x -> x.getValue() > 1)
+                .findFirst().get().getKey();
+        System.out.println(firstRpeated);
+    }
     public static void main(String[] args) {
         String str = "ILoveDawateIslami";
+        System.out.println("first Non Repeated ");
         getFirstNonRepeated(str);
+        System.out.println("first Repeated: ");
+        getFirstRepeated(str);
     }
 }
