92475f926c4583773bc18d0e356d6e1456b1c4a9
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2024-11-23 22:02:43 +0530

    added Transpose of Matrix method

--- JavaPractice/src/com/safatech/dsa/datastructure/array2d/TransposeOfMatrix.java
+++ JavaPractice/src/com/safatech/dsa/datastructure/array2d/TransposeOfMatrix.java
@@ -1,2 +1,48 @@
-package com.safatech.dsa.datastructure.array2d;public class TransposeOfMatrix {
+package com.safatech.dsa.datastructure.array2d;
+
+import java.util.Scanner;
+
+public class TransposeOfMatrix {
+
+    public static void printMatrix(int[][] matrix){
+
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[i].length; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[][] findTranspose(int[][] matrix, int r, int c){
+
+        int[][] ans = new int[c][r];
+        for(int i = 0; i< c; i++){
+            for(int j = 0; j < r; j++){
+                ans[i][j] = matrix[j][i];
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows and columns of matrix.");
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        int[][] matrix = new int[r][c];
+        int totalElements = r * c;
+        System.out.println("Enter " + totalElements + " values");
+
+        for (int i = 0; i < r; i++){
+            for (int j = 0; j < c; j++){
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Input Matrix");
+        printMatrix(matrix);
+        System.out.println("Transpose of Matrix");
+        int[][] transposed = findTranspose(matrix, r, c);
+        printMatrix(transposed);
+    }
 }
