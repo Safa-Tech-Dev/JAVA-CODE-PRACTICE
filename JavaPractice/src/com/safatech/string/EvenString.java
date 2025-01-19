a23f12670c8e886e172d93b876cee3b937ddde3d
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2025-01-19 16:34:14 +0530

    added find even string in string

--- JavaPractice/src/com/safatech/string/EvenString.java
+++ JavaPractice/src/com/safatech/string/EvenString.java
@@ -1,2 +1,24 @@
-package JavaPractice.src.com.safatech.string;public class EvenString {
+package JavaPractice.src.com.safatech.string;
+
+/*
+Write a program to print the even length string
+e.g Hell world we lol
+o/p - hell , we
+ */
+public class EvenString {
+
+    public static void getEvenString(String originalString){
+
+        for(String s : originalString.split(" ")){
+            if(s.length() % 2 == 0){
+                System.out.println(s);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        String givenString = " Hell World We lol";
+        getEvenString(givenString);
+    }
 }
