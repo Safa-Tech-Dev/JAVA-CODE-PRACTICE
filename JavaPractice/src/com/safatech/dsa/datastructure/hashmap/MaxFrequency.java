bd58c1085df8cf18aac378def279c70bc1bbdf68
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2025-01-24 11:25:19 +0530

    Added Find the maximum frequency element method

--- JavaPractice/src/com/safatech/dsa/datastructure/hashmap/MaxFrequency.java
+++ JavaPractice/src/com/safatech/dsa/datastructure/hashmap/MaxFrequency.java
@@ -3,9 +3,9 @@ package JavaPractice.src.com.safatech.dsa.datastructure.hashmap;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MostFrequentElement {
+public class MaxFrequency {
 
-    public static int getMostFrequentElement(int[] arr){
+    public static int getMaxFreuencyElement(int[] arr){
 
         Map<Integer, Integer> map = new HashMap<>();
         for (int el : arr){
@@ -15,9 +15,19 @@ public class MostFrequentElement {
                 map.put(el, map.get(el) + 1);
             }
         }
+        int maxFreq  = 0; int ansKey = -1;
+        for(var el : map.entrySet()){
+            if(el.getValue() > maxFreq){
+                maxFreq = el.getValue();
+                ansKey = el.getKey();
+            }
+        }
+        return ansKey;
     }
     public static void main(String[] args) {
 
+        int[] arr = {2,3,1,2,7,1,4,6,6,7,8,7,7,5,4,3,7,4,3,4,7};
+        System.out.println(getMaxFreuencyElement(arr));
 
     }
 }
