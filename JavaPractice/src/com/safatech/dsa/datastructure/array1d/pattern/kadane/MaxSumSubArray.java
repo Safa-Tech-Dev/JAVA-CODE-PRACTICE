037689c82374ce0ba82975ea3c5e07945b70887a
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2025-05-23 16:49:19 +0530

    Added kadane's algo max sum sub array

---
+++ JavaPractice/src/com/safatech/dsa/datastructure/array1d/pattern/kadane/MaxSumSubArray.java
@@ -0,0 +1,24 @@
+package JavaPractice.src.com.safatech.dsa.datastructure.array1d.pattern.kadane;
+
+public class MaxSumSubArray {
+
+    public static int getMaxSum(int[] arr){
+
+        int currentSum = arr[0]; // initialize index 0 as the current sum
+        int maxSum = arr[0]; // initialize index 0 as the max sum
+
+        // Traverse the over the array starting from index 1
+        for(int i = 1; i < arr.length; i++){
+
+            // Either extend the current subarray or start a new one
+            currentSum = Math.max(arr[i], currentSum + arr[i]);
+            // Update the global maximum sum
+            maxSum = Math.max(maxSum, currentSum);
+        }
+        return maxSum;
+    }
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println("Maximum Subarray Sum = " + getMaxSum(nums)); // Output: 6
+    }
+}
