ad05686db9d6ef18fd285f687e2599d7a478ce16
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2025-05-24 20:34:29 +0530

    refactored the code

--- JavaPractice/src/com/safatech/dsa/datastructure/hashset/MaxNumberOnTable.java
+++ JavaPractice/src/com/safatech/dsa/datastructure/hashset/MaxNumberOnTable.java
@@ -1,2 +1,31 @@
-package JavaPractice.src.com.safatech.dsa.datastructure.hashset;public class MaxNumberOnTable {
+package JavaPractice.src.com.safatech.dsa.datastructure.hashset;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MaxNumberOnTable {
+
+    static int getMaxNumberOnTable(int[] nums){
+
+        Set<Integer> table = new HashSet<>();
+        int max = 0;
+        for(int num : nums){
+
+            if(table.contains(num)){
+                table.remove(num);
+            }else{
+                table.add(num);
+                max = Math.max(max, table.size());
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+
+        int[] num1 = {2,1,1,3,2,3}; // 2
+        int[] num2 = {1,1}; // 1
+        System.out.println(getMaxNumberOnTable(num1));
+        System.out.println(getMaxNumberOnTable(num2));
+
+    }
 }
