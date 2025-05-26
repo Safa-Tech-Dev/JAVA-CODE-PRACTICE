1f6bcb2644d65dd7ae76448caa30e505969ae834
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2025-05-26 19:40:44 +0530

    refactored the code

--- JavaPractice/src/com/safatech/dsa/datastructure/hashmap/IsomorphicString.java
+++ JavaPractice/src/com/safatech/dsa/datastructure/hashmap/IsomorphicString.java
@@ -27,12 +27,9 @@ public class IsomorphicString {
         String t = "add";
 
         System.out.println(isIsomorphic(s, t));
-
         s = "foo";
         t = "bar";
-
         System.out.println(isIsomorphic(s,t));
-
         s = "paper";
         t = "title";
         System.out.println(isIsomorphic(s,t));
@@ -41,10 +38,8 @@ public class IsomorphicString {
     public static boolean isIsomorphic(String s, String t) {
 
         // Two strings cannot be isomorphic if they have different lengths.
-        if(s.length() != t.length()){
+        if(s.length() != t.length())
             return false;
-        }
-
         // Use Java's built in map to store mappings of s chars to chars.
         Map<Character, Character> map = new HashMap<>();
         // Use a set to keep track of already mapped characters.
@@ -54,28 +49,23 @@ public class IsomorphicString {
 
             char c1 = s.charAt(i);
             char c2 = t.charAt(i);
-
             // If c1 has been encountered before
             if(map.containsKey(c1)){
 
-                // Return false if first occurrence of c1 is mapped to
-                // a different character.
-                if(map.get(c1) != c2) {
+                // Return false if first occurrence of c1 is mapped to a different character.
+                if(map.get(c1) != c2)
                     return false;
-                }
             }
             // If c1 is encountered for the first time, it has not been mapped yet:
             else{
                 // Return false if c2 is already mapped to some other char in s
-                if(set.contains(c2)){
+                if(set.contains(c2))
                     return false;
-                }
                 // All checks passed. So insert in the map, and the set.
                 map.put(c1,c2);
                 set.add(c2);
             }
         }
-
         return true;
     }
 }
