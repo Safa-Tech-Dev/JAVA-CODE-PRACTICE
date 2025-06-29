e4bcfcf4b1614720d499be9d4e8b21e3d187af2b
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2025-06-29 20:39:27 +0530

    Added Top K elements method using priority Queue

---
+++ JavaPractice/src/com/safatech/dsa/datastructure/priorityqueue/pattern/topk/TopKLargestElement.java
@@ -0,0 +1,46 @@
+package JavaPractice.src.com.safatech.dsa.datastructure.priorityqueue.pattern.topk;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+//Given an integer array nums and an integer k, return the k largest element in the array.
+// arr = [5,9,3,4,8,2,], k = 3
+// o/p = [5,8,9]
+
+//Time Complexity = O(nlogk)
+//Space Complexity = O(k)
+public class TopKLargestElement {
+
+    public static int[] findTopKLargestElement(int[] nums, int k){
+
+        //1. Create min Heap
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        //2. add first k element in minHeap to maintain the top k elements[T.C = O(klogk]
+        for(int i = 0; i < k; i++){
+            minHeap.add(nums[i]);
+        }
+
+        //3. add remaining elements [T.C = O((n-k) * logk]
+        for(int i = k; i < nums.length; i++){
+
+            minHeap.add(nums[i]); // add the element to heap
+            if(minHeap.size() > k){ // if size is > k, remove the smallest element from the heap
+                minHeap.poll(); // This keeps only k largest element in the minHeap
+            }
+        }
+
+        int[] result = new int[k];
+        for(int i = 0; i < k; i++){
+            result[i] = minHeap.poll();
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {5,9,3,4,8,2};
+        int k = 3;
+        System.out.println(Arrays.toString(findTopKLargestElement(arr,k)));
+    }
+}
