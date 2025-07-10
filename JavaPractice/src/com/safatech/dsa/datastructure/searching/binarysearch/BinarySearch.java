7bc2eecdf0a476812b2a14ddcd2e5bad25d9d3f0
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2025-07-10 14:58:28 +0530

    Added Binary Search both naive and recursive approach

---
+++ JavaPractice/src/com/safatech/dsa/datastructure/searching/binarysearch/BinarySearch.java
@@ -0,0 +1,59 @@
+package JavaPractice.src.com.safatech.dsa.datastructure.searching.binarysearch;
+
+/**
+ * Array must be sorted
+ * Time Complexity : O(logn)
+ */
+public class BinarySearch {
+
+    public static int getBinarySearch(int[] arr, int target){
+
+        int n = arr.length;
+        int  start = 0;
+        int end = n - 1;
+        while (start <= end){
+
+            // To avoid Integer overflow
+            int  mid = start + (end -  start)/2;
+            // if target met return mid
+            if(target == arr[mid]){
+                return mid;
+                // if  target is  smaller, search in left half of array. move end to left
+            }else if(target < arr[mid]){
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1; // not found
+    }
+
+    public static int recursieBinarySearch(int[] arr, int start, int end, int target){
+
+        if(start > end){ // base case
+            return -1;
+        }
+        int mid = start + (end - start)/2; // find the middle of valuet
+        if(target == arr[mid]){
+            return mid; // target found
+        }else if(target < arr[mid]){
+            end = mid - 1;
+            return recursieBinarySearch(arr, start, end, target);
+        }else {
+            start = mid + 1;
+            return recursieBinarySearch(arr, start, end, target);
+        }
+    }
+    public static void main(String[] args) {
+
+        int[] arr = {4,6,8,5,1,2,10,11};
+        int target = 10;
+//        int result = getBinarySearch(arr, target);
+        int result = recursieBinarySearch(arr,0, arr.length - 1, target);
+        if(result != -1){
+            System.out.println("Target found at index: " + result);
+        }else {
+            System.out.println(" Target not found");
+        }
+    }
+}
