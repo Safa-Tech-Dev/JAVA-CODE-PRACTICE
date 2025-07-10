50b8b956c521f1be7e33a87b5a33512e684cbc00
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2025-07-10 14:54:56 +0530

    imported java.util.*

--- JavaPractice/src/com/safatech/java8/SortingElement.java
+++ JavaPractice/src/com/safatech/java8/SortingElement.java
@@ -1,9 +1,6 @@
 package JavaPractice.src.com.safatech.java8;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class SortingElement {
