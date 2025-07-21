93806da7a8e08163011d849d90cf65cd199b2780
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2025-07-21 20:27:17 +0530

    Refactored

---
+++ JavaPractice/src/com/safatech/dsa/datastructure/searching/binarysearch/FirstAndLastOccurence.java
@@ -0,0 +1,4 @@
+package JavaPractice.src.com.safatech.dsa.datastructure.searching.binarysearch;
+
+public class FirstAndLastOccurence {
+}
--- JavaPractice/src/com/safatech/string/Palindrome.java
+++ JavaPractice/src/com/safatech/string/Palindrome.java
@@ -4,6 +4,9 @@ package JavaPractice.src.com.safatech.string;
 Check the String is Palindrome or not
 1.reverse the string and check with the original string
 2. if contains equal , palindrom otherwise not palindrom
+Allocates memory for a new reversed string.
+Slightly less efficient for large strings due to object creation.
+## Two Pointer is the better approach
 */
 public class Palindrome {
 
@@ -15,7 +18,7 @@ public class Palindrome {
     }
     public static void main(String[] args) {
 
-//        String originalString = "Mom";
+        String originalString1 = "Mom";
         String originalString = "java";
 
         if(originalString.equalsIgnoreCase(getReversedString(originalString))){
