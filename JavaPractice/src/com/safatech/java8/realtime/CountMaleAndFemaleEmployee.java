ecb839b1a94b2bf3b17d25ff76ee3bbcf85e1928
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2025-08-24 18:35:55 +0530

    Find out the count of male and female employees present in the organization

---
+++ JavaPractice/src/com/safatech/java8/realtime/CountMaleAndFemaleEmployee.java
@@ -0,0 +1,30 @@
+package JavaPractice.src.com.safatech.java8.realtime;
+
+import JavaPractice.src.com.safatech.comparator.Employee;
+import JavaPractice.src.com.safatech.java8.model.EmployeeRepositoryRealTime;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * Find out the count of male and female employees present in the organization
+*/
+public class CountMaleAndFemaleEmployee {
+
+
+    public static void getCountOfMaleFemale(List<Employee> employeeList){
+
+        Map<String, Long> maleFemaleCount = employeeList.stream()
+                .collect(Collectors.groupingBy(Employee::getGender, Collectors.counting()));
+
+        System.out.println(maleFemaleCount);
+    }
+
+    public static void main(String[] args) {
+
+        List<Employee> employees = EmployeeRepositoryRealTime.getEmployee();
+        getCountOfMaleFemale(employees);
+    }
+}
