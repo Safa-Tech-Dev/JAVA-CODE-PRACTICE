b6e7834d3f81a1b684688b249cda2851fbc1452a
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2025-08-26 20:07:06 +0530

    Added 1679 - Max Number of K-Sum Pairs using HashMap optimal solution

--- JavaPractice/src/com/safatech/dsa/datastructure/array1d/pattern/twopointerers/MaxOperations.java
+++ JavaPractice/src/com/safatech/dsa/datastructure/array1d/pattern/twopointerers/MaxOperations.java
@@ -1,12 +1,15 @@
 package JavaPractice.src.com.safatech.dsa.datastructure.array1d.pattern.twopointerers;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 1679. Max Number of K-Sum Pairs
  */
 public class MaxOperations {
 
+    // Two Pointer
     public static int maxOperations(int[] arr, int target ){
 
         int moves = 0;
@@ -29,6 +32,27 @@ public class MaxOperations {
         }
         return moves;
     }
+
+    // Using HashMap
+    public static int maxOperations2(int[] arr, int target){
+
+        Map<Integer, Integer> freq = new HashMap<>();
+        int pairs = 0;
+        for(int num : arr){
+
+            int complement = target - num;
+            if(freq.containsKey(complement)){
+                pairs++;
+                if(freq.get(complement) == 1) // if complement value is 1, remove from the map
+                    freq.remove(complement);
+                else
+                    freq.put(complement, freq.get(complement) - 1); // or decrease the value by 1
+            }else{
+                freq.put(num, freq.getOrDefault(num, 0) + 1);
+            }
+        }
+        return pairs;
+    }
     public static void main(String[] args) {
 
         int[] arr = {1,2,3,4};
@@ -38,5 +62,7 @@ public class MaxOperations {
         int[] arr2 = {3,1,3,4,3};
         int target2 = 6;
         System.out.println(maxOperations(arr2, target2));
+        System.out.println(maxOperations2(arr, target));
+        System.out.println(maxOperations2(arr2, target2));
     }
 }
