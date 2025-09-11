75af41cc5133a10bed34f01a5138c8e9d9271374
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2025-09-11 16:30:59 +0530

    modified the Fibonacci series  and recursive call

--- JavaPractice/src/com/safatech/corejava/Fibonacci.java
+++ JavaPractice/src/com/safatech/corejava/Fibonacci.java
@@ -6,6 +6,20 @@ package com.safatech.corejava;
 
 public class Fibonacci {
 
+    public static void getFiboSeries(int n){
+
+        int first = 0;
+        int second = 1;
+
+        for(int i = 0; i < n; i++){
+
+            System.out.print(first + ",");
+            int next = first + second; // c = 0 + 1 = 1
+            first = second;  // a = 1
+            second = next; // b = 1
+        }
+    }
+
     public static int getNthFibonacci(int n){
 
         if(n <= 1)
@@ -15,7 +29,15 @@ public class Fibonacci {
 
     public static void main(String[] args) {
 
-        int num = 10;
+        int num = 5;
         System.out.println(getNthFibonacci(num));
+
+        for(int i = 0; i < num; i++){
+
+            System.out.print(getNthFibonacci(i) + " ");
+        }
+//        int n = 5;
+        System.out.println();
+        getFiboSeries(num);
     }
 }
