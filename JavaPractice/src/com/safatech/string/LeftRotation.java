3029c5ad21390ea1730a68dbdb70ff6c0a194d99
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2025-09-27 18:21:27 +0530

    Left rotation of a string by rotation factor

---
+++ JavaPractice/src/com/safatech/string/LeftRotation.java
@@ -0,0 +1,22 @@
+package JavaPractice.src.com.safatech.string;
+
+import JavaPractice.src.com.safatech.corejava.CoreJava;
+
+public class LeftRotation {
+
+    public static String rotateLeft(String str, int rotationFactor){
+
+        /**
+         *  decode
+         * substring(rotationFactor) + substring(0,rotationFactor)
+         * code   + de = codede
+         */
+
+        String rotatedString = str.substring(rotationFactor) + str.substring(0, rotationFactor);
+        return rotatedString;
+    }
+    public static void main(String[] args) {
+
+        System.out.println(rotateLeft("decode",2));
+    }
+}
