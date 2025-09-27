c7a190527620e08451b47066d9bf97720ebb1fa8
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2025-09-27 19:16:23 +0530

    added get largest method

--- JavaPractice/src/com/safatech/dsa/datastructure/array1d/SmallestNLargestElement.java
+++ JavaPractice/src/com/safatech/dsa/datastructure/array1d/SmallestNLargestElement.java
@@ -1,4 +1,4 @@
-package com.safatech.dsa.datastructure.array1d;
+package JavaPractice.src.com.safatech.dsa.datastructure.array1d;
 
 import java.util.Arrays;
 
@@ -10,10 +10,24 @@ public class SmallestNLargestElement {
         int[] ans = {arr[0], arr[arr.length - 1]};
         return ans;
     }
+
+    public static int getLargest(int[] arr){
+
+        int largest = arr[0];
+
+        for(int i = 1; i < arr.length; i++){
+
+            if(arr[i] > largest){
+                largest = arr[i];
+            }
+        }
+        return largest;
+    }
     public static void main(String[] args) {
 
         int[] arr = {0,1,3,5,6,8,9};
         System.out.println(Arrays.toString(smallestNLargestElement(arr)));
+        System.out.println(getLargest(arr));
 
     }
 }
