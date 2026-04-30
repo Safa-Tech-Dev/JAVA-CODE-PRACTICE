25569afe138b097b295d69ff60390aa6039813da
saddam <saddam8101.sk@gmail.com>
2026-04-30 22:41:03 +0530

    added new method for input array not sorted, hash map approach

--- JavaPractice/src/com/safatech/dsa/datastructure/array1d/pattern/twopointerers/TwoSumIndex.java
+++ JavaPractice/src/com/safatech/dsa/datastructure/array1d/pattern/twopointerers/TwoSumIndex.java
@@ -1,9 +1,12 @@
 package JavaPractice.src.com.safatech.dsa.datastructure.array1d.pattern.twopointerers;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSumIndex {
 
+    // Input array is sorted, Use two approach
     public static int[] getTwoSumIndex(int[] arr, int target){
 
         int n = arr.length;
@@ -25,11 +28,28 @@ public class TwoSumIndex {
         }
         return new int[0]; // no sum match
     }
+    public static int[] getTwoSumIndexNotSortedArray(int[] arr, int target){
+
+        int n = arr.length;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < n; i++){
+
+            int pair = target - arr[i];
+            if(map.containsKey(pair) && map.get(pair) != i){
+
+                return new int[]{map.get(pair),i};
+            }
+            map.put(arr[i], i);
+        }
+        return new int[0];
+    }
 
     public static void main(String[] args) {
 
         int[] arr = {2,7,11,15};
         int target = 9;
         System.out.println("Target Sum: " + Arrays.toString(getTwoSumIndex(arr, target)));
+        IO.println(Arrays.toString(getTwoSumIndexNotSortedArray(arr,target)));
     }
 }
