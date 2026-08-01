79d6c590577eac3432f85ad63ba99eadb12991a9
saddam <saddam8101.sk@gmail.com>
2026-08-01 20:32:45 +0530

    Added Generic Extend

---
+++ JavaPractice/src/com/safatech/generics/GenericExtend.java
@@ -0,0 +1,7 @@
+package JavaPractice.src.com.safatech.generics;
+
+// for generic extends no need to define the type, Type can be defined at the time obj creation
+public class GenericExtend<T> extends Print<T> {
+
+
+}
